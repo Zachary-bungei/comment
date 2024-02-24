c8b16d4d369ac7b264ff0c9a6ea67285828188ea
Zachary-bungei <160045464+Zachary-bungei@users.noreply.github.com>
2024-02-24 08:52:58 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById( 'commentForm' );
+form. addEventListener ('submit', function (event) {
+event. preventDefault();
+const senderEmail = document. getElementById( ' senderEmail' ).value;
+const comment = document. getElementById( 'comment' ).value;
+민
+// Send email to Zacharia.chemoiwo@mpesafoundationacademy.ac.ke
+console. log(Email Sent to Zacharia. chemoiwo@mpesafoundationacademy.ac.ke from ${senderEmail}: ${comment}*);
+// You can add code here to actually send the email
+Сору
+Revert
+｝）；
